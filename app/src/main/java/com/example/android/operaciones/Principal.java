0cff1d0fbc07e8a5475dfbc120c2842f81717e27
damianm <damian3095007@gmail.com>
2017-08-22 20:04:38 -0500

    INTERFAZ TERMINADA OPERACIONES

--- app/src/main/java/com/example/android/operaciones/Principal.java
+++ app/src/main/java/com/example/android/operaciones/Principal.java
@@ -4,13 +4,17 @@ import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
  private TextView res;
     private EditText n1, n2;
     private Resources resources;
+    private Spinner operaciones;
+    private String op[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,32 +23,43 @@ public class Principal extends AppCompatActivity {
         res=(TextView)findViewById(R.id.lblresultado);
         n1=(EditText)findViewById(R.id.txtnumero1);
         n2=(EditText)findViewById(R.id.txtnumero2);
+        operaciones=(Spinner) findViewById(R.id.cbxoperaciones);
         resources=this.getResources();
-    }
 
+        op=resources.getStringArray(R.array.operacionescal);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,op);
+        operaciones.setAdapter(adapter);
+
+
+    }
+       public boolean Validar(){
+           if (n1.getText().toString().isEmpty()){
+               n1.setError(resources.getString(R.string.Mensaje_error_uno));
+               return false;
+           } if ( n2.getText().toString().isEmpty()){
+               n2.setError(resources.getString(R.string.Mensaje_error_dos));
+               return false;
+           }
+           return true;
+
+     }
     public void calcular(View v){
         int num1,num2,resl;
-        num1 =Integer.parseInt(n1.getText().toString());
-        num2 =Integer.parseInt(n2.getText().toString());
 
-        if (n1.getText().toString().isEmpty()){
-            n1.setError(resources.getString(R.string.Mensaje_error_uno));
-        } if ( n2.getText().toString().isEmpty()){
-            n2.setError(resources.getString(R.string.Mensaje_error_dos));
-        }else{
-            resl=num1+num2;
-            res.setText(resl+"");
+
+           if(Validar()) {
+               num1 =Integer.parseInt(n1.getText().toString());
+               num2 =Integer.parseInt(n2.getText().toString());
+    resl = num1 + num2;
+    res.setText(resl + "");
+          }
         }
-
-
-
-    }
-
-    public void borrarr(View v){
+ public void borrarr(View v){
         n1.setText("");
         n2.setText("");
         res.setText("");
         n1.requestFocus();
     }
+
 
 }
